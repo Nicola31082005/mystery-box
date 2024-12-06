65fc4ac304caeeab033b67e72869df6f9c6db68b
Nicola31082005 <nikule512@gmail.com>
2024-12-06 17:59:04 +0200

    Update layoutMiddleware.js

--- src/middleware/layoutMiddleware.js
+++ src/middleware/layoutMiddleware.js
@@ -6,13 +6,11 @@ const layoutTemplate = (body) => html`
 <div class="bg-white">
   <header class="absolute inset-x-0 top-0 z-50">
     <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-      
-    <div class="flex lg:flex-1">
+      <div class="flex lg:flex-1">
         <a href="/" class="">
           <span>Mystery Box</span>
         </a>
       </div>
-
       <div class="hidden lg:flex lg:gap-x-12">
         <a href="/boxes" class="text-sm/6 font-semibold text-gray-900">Boxes</a>
         <a href="/features" class="text-sm/6 font-semibold text-gray-900">Features</a>
@@ -21,9 +19,11 @@ const layoutTemplate = (body) => html`
         <a href="#" class="text-sm/6 font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
       </div>
     </nav>
+  </header>
+  ${body}
+</div>
+`;
 
-    ${body}
-`
 
 export function layoutView(ctx, next) {
     
